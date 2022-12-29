d52b8ce017d27601f207ec3915b392f38fb9ca9f
bekmarc <104980306+bekmarc@users.noreply.github.com>
2022-12-29 15:02:15 +0000

    is-new-commit

--- apps/ngshop/src/app/app.component.ts
+++ apps/ngshop/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngshop-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngshop';
