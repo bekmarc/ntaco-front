cac5e79ea036ae9de43ffb77b387f8ffd480b59f
bekmarc <104980306+bekmarc@users.noreply.github.com>
2023-01-04 17:05:16 +0000

    isYooo

--- libs/orders/src/index.ts
+++ libs/orders/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/orders.module';
+export * from './lib/orders.module';   
--- libs/orders/src/lib/orders.module.ts
+++ libs/orders/src/lib/orders.module.ts
@@ -4,4 +4,4 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   imports: [CommonModule],
 })
-export class OrdersModule {}
+export class OrdersModule {}  
