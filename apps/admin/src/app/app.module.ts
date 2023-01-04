179e72f10798a3212914504b958129e7c7f71b70
BEKMAN <bekoumarco123@gmail.com>
2023-01-04 18:50:41 +0000

    new-for-commit-global
    is Zoooo!

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -6,9 +6,17 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    DashboardComponent,
+    ShellComponent,
+    SidebarComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
--- apps/admin/src/app/app.routes.ts
+++ apps/admin/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [{
+  path:'',
+  component: ShellComponent,
+  children: [
+    {
+      path: '',
+      component: DashboardComponent
+    }
+  ]
+}];
